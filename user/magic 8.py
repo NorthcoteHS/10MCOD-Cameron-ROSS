ec883dc3620287f8761f7acb637f5cea9c56fd0e
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-03-01 12:32:40 +1100

    ROSS, Cameron: 2018-03-01 12:32:40

---
+++ user/magic 8.py
@@ -0,0 +1,22 @@
+from random import randint
+magic=randint(0,9)
+input ('')
+print (randint(0,9))
+if magic==1:
+    print ("You will be okay")
+if magic==2:
+    print ("Soon you will find out but not today")
+if magic==3:
+    print ("Yes")
+if magic==4:
+    print ("No")
+if magic==5:
+    print ("You are wise and will give the advice")
+if magic==6:
+    print ("WHY ARE YOU DOING THIS")
+if magic==7:
+    print ("You are not wise")
+if magic==8:
+    print ("Family is the key")
+if magic==9:
+    print ("Hello")
